fe7b413295df8e83a5ccac2ae7a14fd4470ba817
ankur0145 <Ankur.sharma@Nextdynamix.com>
2023-07-20 10:54:13 +0530

    new commit

--- blog/api/models.py
+++ blog/api/models.py
@@ -4,6 +4,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     body = models.TextField(blank=True, default='')
+    name=models.CharField(max_length=10,default=True)
     owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
 
     class Meta:
